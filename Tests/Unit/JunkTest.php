5c7789de3a19546dd38ff40334fa6e938d45c4c0
aejsmont <ejsmont.artur@gmail.com>
2013-03-17 22:22:43 +1100

    added doctrine cache based service and a test

--- Tests/Unit/JunkTest.php
+++ Tests/Unit/JunkTest.php
@@ -7,6 +7,17 @@ use Ejsmont\CircuitBreaker\Core\CircuitBreaker;
 
 class DoctrineCacheAdapterTest extends \PHPUnit_Framework_TestCase {
 
+    /**
+     * @var ContainerBuilder
+     */
+    private $container;
+
+    public function setup() {
+        parent::setUp();
+        $this->container = new ContainerBuilder();
+        $this->container->setParameter("kernel.cache_dir", dirname(__FILE__) . "/../../build/cache");
+    }
+
     public function testBundle() {
         $inst = new \Ejsmont\CircuitBreakerBundle\EjsmontCircuitBreakerBundle();
         $this->assertTrue(!empty($inst));
@@ -18,12 +29,29 @@ class DoctrineCacheAdapterTest extends \PHPUnit_Framework_TestCase {
     }
 
     public function testLoadApcExtension() {
-        $container = new ContainerBuilder();
         $inst = new \Ejsmont\CircuitBreakerBundle\DependencyInjection\EjsmontCircuitBreakerExtension();
-        $inst->load(array(), $container);
-        $cb = $container->get('apcCircuitBreaker');
+        $inst->load(array(), $this->container);
+        $cb = $this->container->get('apcCircuitBreaker');
 
         $this->assertTrue($cb instanceof CircuitBreaker);
     }
 
+    public function testLoadDefaultExtension() {
+        $inst = new \Ejsmont\CircuitBreakerBundle\DependencyInjection\EjsmontCircuitBreakerExtension();
+        $inst->load(array(), $this->container);
+        $cb = $this->container->get('circuitBreaker');
+        $this->assertTrue($cb instanceof CircuitBreaker);
+
+        // before its all ok
+        $this->assertTrue($cb->isAvailable('testFakeService'));
+        // mark as failed
+        for ($i = 1; $i <= 30; $i++) {
+            $cb->reportFailure('testFakeService');
+        }
+        // after fails but others work
+        $this->assertFalse($cb->isAvailable('testFakeService'));
+        $this->assertTrue($cb->isAvailable('anotherFakeService'));
+    }
+
 }
+
